bbe6122dbf2f6e3c41f4132059615fa52ba1c432
Arian Abedanzadeh <arianabedan@gmail.com>
2021-08-02 17:12:56 -0400

    Added new Url Format

--- express_server.js
+++ express_server.js
@@ -1,17 +1,45 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+const generateRandomString = () => {
+  let outStr = "";
+  while (outStr.length < 6) {
+    let randCharCode = Math.floor(Math.random() * 122 + 48);
+    //number
+    if (randCharCode < 58) {
+      outStr += String.fromCharCode(randCharCode);
+    //cap and lower letters
+    } else if ((randCharCode >= 65 && randCharCode <= 90) || (randCharCode >= 97 && randCharCode <= 122)) {
+      outStr += String.fromCharCode(randCharCode);
+    }
+  }
+  return outStr;
+};
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  console.log(req.body);
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
